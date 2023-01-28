74ac9a0b95caaaf8fcd91de3972838af7325b391
Justin Kemp <jkemp0790@gmail.com>
2023-01-28 15:42:27 -0600

    built mobile navbar and website header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,61 @@
-import React from 'react';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 import logo from '../../assets/jk-logo.svg';
 
+// const Menu = () => (
+//   <>
+//   <p><a href='#home'>Home</a></p>
+//   <p><a href='#wjgk3'>What is JGK3?</a></p>
+//   <p><a href='#possibility'>Open AI</a></p>
+//   <p><a href='#features'>Case Studies</a></p>
+//   <p><a href='#blog'>Library</a></p>
+//   </>
+// )
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="jgk3_navbar">
       <div className='jgk3__navbar-links'>
         <div className='jgk3__navbar-links_logo'>
-          <img src={logo} alt="logo" />
+          <img src={logo} />
+        </div>
+        <div className='jgk3__navbar-links_container'>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#wjgk3'>What is JGK3?</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
         </div>
       </div>
+      <div className='jgk3__navbar-sign'>
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
+      <div className='jgk3__navbar-menu'>
+        {toggleMenu
+          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className='jgk3__navbar-menu_container scale-up-center'>
+          <div className='jgk3__navbar-menu_container-links'>
+            <p><a href='#home'>Home</a></p>
+            <p><a href='#wjgk3'>What is JGK3?</a></p>
+            <p><a href='#possibility'>Open AI</a></p>
+            <p><a href='#features'>Case Studies</a></p>
+            <p><a href='#blog'>Library</a></p>
+          </div>
+          <div className='jgk3__navbar-menu_container-links-sign'>
+            <p>Sign In</p>
+            <button type="button">Sign Up</button>
+          </div>
+        </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './header.css';
+import world from '../../assets/world-logo2.png';
 
 const Header = () => {
   return (
-    <div>
-        Header
+    <div className='jgk3__header section__padding' id='home'>
+        <div className='jgk3__header-content'>
+          <h1 className='gradient__text'>Hey There! Let's create something incredible together!</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa ultricies mi quis hendrerit dolor magna eget est.</p>
+          <div className='jgk3__header-content__input'>
+            <input type='email' placeholder='Your Email Address'></input>
+            <button type='button'>Get Started</button>
+          </div>
+          {/* <div className='jgk3__header-content__people'>
+            <img src={people} />
+          </div> */}
+          <div className='jgk3__header-image'>
+            <img src={world} alt='world'/>
+          </div>
+        </div>
     </div>
   )
 }
